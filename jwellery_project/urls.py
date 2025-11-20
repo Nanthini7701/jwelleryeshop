486fa6307bfe7bde8d3f382fe28309f1950989cc
Nanthini Palani <48705904+Nanthini7701@users.noreply.github.com>
2025-11-20 14:47:34 +0530

    Add static file serving for development

--- jwellery_project/urls.py
+++ jwellery_project/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
     path('', include('django.contrib.auth.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
